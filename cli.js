ef7d90e7dcc4ad85f0c380684a84605a33b7e068
Luke Watts <luke@thisis.la>
2016-10-29 16:48:10 +0100

    Moves core of tool away from the cli.js interface

--- cli.js
+++ cli.js
@@ -1,274 +1,43 @@
 #!/usr/bin/env node
 
+const Browsersnap = require('./src/Browsersnap.js');
+
 const chalk = require('chalk'),
-		got = require('got'),
 		fs = require('fs'),
 		_ = require('lodash'),
 		BrowserStack = require('browserstack'),
 		pkg = require('./package.json'),
 		program = require('commander');
 
-var	stdout = process.stdout;
-var auth = getAuthDetails();
-var screenshotClient = BrowserStack.createScreenshotClient(auth);
-
-var screenshotQueue = [];
-
-var processingQueue = [];
-var processingController;
-
-var processedCounter = 0;
-var processTotal = 0;
+program
+	.version(pkg.version)
+	.option('-u, --username [string]', 'Username')
+	.option('-p, --password [string]', 'Password')
+	.option('--verbose', 'Verbose mode');
 
 program
-	.version(pkg.version);
+	.command('browsers')
+	.description(`Fetch available browsers as json`)
+	.action(function(filter, options) {
 
-program
-	.command('browsers [latest]')
-	.description(`Fetch available browsers as json.
-		Optionally append 'latest' to only get the most recent version.`)
-	.action(function(filter) {
+		let bs = new Browsersnap(program.username, program.password, {
+			verboseMode : program.verbose
+		});
 
-	screenshotClient.getBrowsers(function( error, browsers ) {
-
-		if (filter === 'latest') {
-			var nested = [];
-			var tmpB = '', tmpBv = 0;
-
-			_.map(browsers.reverse(), function(browser) {
-				var os = keyify(browser.os);
-				var osVersion = keyify(browser.os_version);
-				var browserName = keyify(browser.browser);
-				var browserVersion = keyify(browser.browser_version);
-
-				if ( isDepreciatedOS(os, osVersion) || (tmpB === browserName && tmpBv > browserVersion) ) {
-					return;
-				}
-
-				tmpB = browserName;
-				tmpBv = browserVersion;
-
-				if (nested.length < 25 ) {
-					nested.push(browser);
-				}
-			});
-
-			console.log( JSON.stringify(nested));
-
-
-		} else {
-			console.log( JSON.stringify(browsers) );
-		}
+		return bs.browsers();
 	});
-});
 
 program
 	.command('get <url|comma,seperated,list,of,urls|path-to-json.url>')
 	.description('Screenshot requested url')
-	.action( function( url ) {
+	.action( function( urls ) {
 
-		// var urls = _.slice( getUrlsFromString( url ), 0, 2 );
-		var urls = getUrlsFromString( url );
-
-		console.log( chalk.cyan('Fetching ' + (urls.length > 1 ? urls.length + ' screenshots.' : url) ) );
-
-		urls.map(function(uri) {
-
-			screenshotQueue.push(uri);
+		let bs = new Browsersnap(program.username, program.password, {
+			verboseMode : program.verbose
 		});
 
-		processingController = setInterval( checkingProcessingJobs, 1500 );
-
-		processTotal = urls.length;
+		bs.get(urls);
 
 	});
 
 program.parse(process.argv);
-
-/**
- *
- * @return array - Array of URLs
- */
-function getUrlsFromString( url ) {
-
-	if ( url.match(/json/) ) {
-
-		return require( url ).urls;
-
-	}
-
-	return url.split(',');
-}
-
-function getLatestBrowsers( numberOfBrowsers ) {
-
-	return [{"device":null,"browser":"firefox","os_version":"El Capitan","browser_version":"45.0","os":"OS X"}];
-
-//	return _.slice( _.shuffle( require( BROWSER_PATH + 'latest.json')), 0, numberOfBrowsers );
-}
-
-function isDepreciatedOS( os, osVersion ) {
-	var unsupported_os_versions = ['xp','snowleopard','mountainlion','lion'];
-
-	if ( ['windows','osx'].indexOf(os) > -1 && unsupported_os_versions.indexOf(osVersion) > -1 ) {
-		return true;
-	}
-
-	if ( ['android'].indexOf(os) > -1 ) {
-		return osVersion <= 43;
-	}
-
-	if ( ['ios'].indexOf(os) > -1 ) {
-		return osVersion <= 60;
-	}
-
-	return false;
-}
-
-function keyify( str ) {
-
-	if (parseFloat(str)) {
-		return str.replace('.','');
-	}
-
-	return str ? str.toLowerCase().replace(/\s/g,'') : false;
-}
-
-function getScreenshotFor( url, browsers ) {
-	var opts = {
-		url 		: url,
-		browsers	: browsers,
-		wait_time 	: 10
-	};
-
-	screenshotClient.generateScreenshots( opts, (err, job) => {
-		if (err) {
-			console.log(chalk.red(err));
-			screenshotQueue.push(url);
-			console.log( chalk.yellow( `Moved ${url} back to screenshotQueue` ) );
-
-			return false;
-		}
-
-		// TODO: What is this all doing?
-		//
-		// if (job.screenshots) {
-		// 	for (var i = 0; i < job.screenshots.length; i++) {
-		// 		screenshotQueue.push(job.screenshots[i]);
-		// 	}
-		// }
-
-		processingQueue.push( job );
-		console.log('Added another job to processingQueue so there are now:', processingQueue.length );
-	});
-}
-
-
-
-function getAuthDetails() {
-	var ret = false;
-
-	if ( fs.existsSync('.browserstack') ) {
-		ret = JSON.parse(fs.readFileSync('.browserstack'));
-	} else {
-		stdout.write('No Auth details provided, check you\'ve got a .browserstack file.\n');
-		process.exit();
-	}
-
-	return ret;
-}
-
-
-/**
- * [saveScreenshots description]
- * @param  {array} screenshots
- * @return {[type]}             [description]
- */
-function saveScreenshots(screenshots) {
-
-	console.log('Screenshots:', screenshots );
-
-	screenshots.map(function(screenshot) {
-
-		var folder = 'reports/hauslondon.com/';
-
-		console.log('Saving image for:', screenshot );
-
-		if (!fs.existsSync(folder) ) {
-			fs.mkdir(folder);
-		}
-
-		var filename = _.compact( _.values( _.pick( screenshot, ['os','os_version','device','browser','browser_version']))).join('_').toLowerCase();
-
-		console.log('Writing to:', screenshot.image_url );
-
-		got.stream(screenshot.image_url).pipe( fs.createWriteStream( folder + '/' + urlToFilename( screenshot.url ) + '-' + filename +'.jpg',{
-			autoclose: false
-		}));
-
-	});
-}
-
-var urlToFilename = ( url ) => {
-	return url.replace(/https?:\/\/|\/$/g,'').split('?').shift().replace(/\//g,'-');
-};
-
-function checkingProcessingJobs() {
-
-	// console.log( chalk.yellow( 'Number of items to be sent to browserstack:', screenshotQueue.length ) );
-	// console.log( chalk.yellow( 'Number of items awaiting completion by browserstack:', processingQueue.length ) );
-
-	console.log( chalk.green( processedCounter + ' of ' + processTotal + ' completed.'));
-
-	if ( processingQueue.length >= 1 ) {
-		getJobStatus( processingQueue[0].job_id );
-		return;
-	}
-
-
-	// Check if there are
-	//
-	if ( screenshotQueue.length > 0 ) {
-		getScreenshotFor( screenshotQueue.pop(), getLatestBrowsers( 2 ) );
-		return;
-	}
-
-	console.log( chalk.bgGreen( 'All processing jobs complete.' ) );
-	clearInterval( processingController );
-}
-
-function getJobStatus(job_id) {
-
-	// console.log( 'Checking job:', job_id );
-
-	screenshotClient.getJob( job_id ,function(err, job) {
-
-		if (err) {
-			throw err;
-		}
-
-		if (job.state === 'all_queued') {
-
-			console.log( chalk.dim('Waiting: ' + job.state ), job );
-
-		} else {
-
-			// console.log('Job Status:', job.state );
-
-			if (job.state === 'done' ) {
-				processedCounter++;
-				console.log(chalk.bgGreen('Job 1 of ' + processingQueue.length + ' completed.'));
-
-				console.log('Remove job from queue:', job.id );
-
-				processingQueue = _.remove( processingQueue, function(j) {
-					console.log('Removing:', j.id, job.id );
-					return j.id === job.id;
-				});
-
-				console.log('Number of jobs remaining:', processingQueue.length );
-				saveScreenshots( job.screenshots );
-			}
-		}
-	});
-}
---
+++ src/browsersnap.js
@@ -0,0 +1,300 @@
+const chalk = require('chalk');
+const got = require('got');
+const fs = require('fs');
+const Browserstack = require('browserstack');
+const _ = require('lodash');
+
+
+function getAuthDetails() {
+	var ret = false;
+
+	if ( fs.existsSync( process.cwd() + '/.browserstack' ) ) {
+		ret = JSON.parse(fs.readFileSync( process.cwd() + '/.browserstack'));
+	} else {
+		console.log(chalk.yellow(
+			`No Browserstack account details provided, either:
+* Check you've got a .browserstack file in current directory
+* Add them to this command with --username|password flags
+		`));
+		process.exit();
+	}
+
+	return ret;
+}
+
+/**
+ *
+ * @return array - Array of URLs
+ */
+function parseUrls( url ) {
+
+	if ( url.match(/json/) ) {
+
+		return require( url ).urls;
+	}
+
+	if (url.match(',')) {
+		return url.split(',');
+	}
+
+	return [url];
+}
+
+const Browsersnap = function( username, password, options ) {
+
+	this.auth = false;
+
+	this.opts = options;
+	this.screenshotQueue = [];
+	this.processingQueue = [];
+	this.processingController;
+	this.processedCounter = 0;
+	this.processTotal = 0;
+	this.urls = [];
+	this.imagesOutputTo = 'reports/';
+
+	this.client;
+
+	if ( !username || !password) {
+		this.auth = getAuthDetails();
+	} else {
+		this.auth = {
+			username : username,
+			password : password
+		}
+	}
+
+	if (!this.auth) {
+
+		console.log(chalk.red('Failed to find access credentials.'));
+
+		return this;
+	}
+
+	this.client = Browserstack.createScreenshotClient(this.auth)
+
+	this.log(chalk.green(`Using account ${this.auth.username}.`));
+
+	return this;
+}
+
+
+/**
+ *
+ *
+ * @param  array urls
+ * @return array      [description]
+ */
+Browsersnap.prototype.get = function(urls) {
+
+	let _self = this;
+	_self.urls = parseUrls(urls);
+
+	console.log( chalk.green(`${_self.urls.length} url(s) requested.`));
+
+
+	_self.urls.map(function(uri) {
+		_self.screenshotQueue.push(uri);
+	});
+
+	_self.processingController = setInterval( function() {
+		_self.checkingProcessingJobs();
+	}, 1500 );
+
+	return urls;
+}
+
+Browsersnap.prototype.browsers = function() {
+	let _self = this;
+
+	_self.client.getBrowsers(function( error, browsers ) {
+
+		if (error) {
+			console.log(chalk.red(error));
+			return;
+		}
+
+		// if (filter === 'latest') {
+		// 	var nested = [];
+		// 	var tmpB = '', tmpBv = 0;
+
+		// 	_.map(browsers.reverse(), function(browser) {
+		// 		var os = keyify(browser.os);
+		// 		var osVersion = keyify(browser.os_version);
+		// 		var browserName = keyify(browser.browser);
+		// 		var browserVersion = keyify(browser.browser_version);
+
+		// 		if ( isDepreciatedOS(os, osVersion) || (tmpB === browserName && tmpBv > browserVersion) ) {
+		// 			return;
+		// 		}
+
+		// 		tmpB = browserName;
+		// 		tmpBv = browserVersion;
+
+		// 		if (nested.length < 25 ) {
+		// 			nested.push(browser);
+		// 		}
+		// 	});
+
+		// 	console.log( JSON.stringify(nested));
+
+
+		// } else {
+		console.log( JSON.stringify(browsers) );
+	});
+}
+
+Browsersnap.prototype.checkingProcessingJobs = function() {
+
+	let _self = this;
+
+
+	if ( _self.processingQueue.length >= 1 ) {
+		_self.getJobStatus( _self.processingQueue[0].job_id );
+
+		_self.log( chalk.green( _self.processedCounter + ' of ' + _self.urls.length + ' completed.'));
+
+		return;
+	}
+
+
+	if ( _self.screenshotQueue.length > 0 ) {
+		_self.getScreenshotFor( _self.screenshotQueue.pop(), _self.getLatestBrowsers( 2 ) );
+		return;
+	}
+
+	console.log( chalk.green(
+		`All processing jobs complete 👌
+Images saved in ${_self.imagesOutputTo}/*.jpg` ) );
+	clearInterval( _self.processingController );
+}
+
+Browsersnap.prototype.getJobStatus = function(job_id) {
+
+	let _self = this;
+
+	_self.client.getJob( job_id ,function(err, job) {
+
+		if (err) {
+			throw err;
+		}
+
+		if (job.state === 'all_queued') {
+
+			console.log( chalk.dim('Waiting: ' + job.state ), job );
+
+		} else {
+
+			if (job.state === 'done') {
+
+				_self.processedCounter++;
+
+				_self.processingQueue = _.remove( _self.processingQueue, function(j) {
+					return j.id === job.id;
+				});
+
+				_self.saveScreenshots( job.screenshots );
+			}
+		}
+	});
+}
+
+
+
+Browsersnap.prototype.getLatestBrowsers = function( numberOfBrowsers ) {
+
+	return [{"device":null,"browser":"firefox","os_version":"El Capitan","browser_version":"45.0","os":"OS X"}];
+
+	return _.slice( _.shuffle( require( BROWSER_PATH + 'latest.json')), 0, numberOfBrowsers );
+}
+
+function isDepreciatedOS( os, osVersion ) {
+	var unsupported_os_versions = ['xp','snowleopard','mountainlion','lion'];
+
+	if ( ['windows','osx'].indexOf(os) > -1 && unsupported_os_versions.indexOf(osVersion) > -1 ) {
+		return true;
+	}
+
+	if ( ['android'].indexOf(os) > -1 ) {
+		return osVersion <= 43;
+	}
+
+	if ( ['ios'].indexOf(os) > -1 ) {
+		return osVersion <= 60;
+	}
+
+	return false;
+}
+
+function keyify( str ) {
+
+	if (parseFloat(str)) {
+		return str.replace('.','');
+	}
+
+	return str ? str.toLowerCase().replace(/\s/g,'') : false;
+}
+
+Browsersnap.prototype.log = function(str) {
+	if (this.opts.verboseMode) {
+		console.log(str);
+	}
+}
+
+Browsersnap.prototype.getScreenshotFor = function ( url, browsers ) {
+
+	let _self = this,
+		opts = {
+		url 		: url,
+		browsers	: browsers,
+		wait_time 	: 10
+	};
+
+	_self.client.generateScreenshots( opts, (err, job) => {
+		if (err) {
+			console.log(chalk.red(err), err);
+			_self.screenshotQueue.push(url);
+
+			_self.log(chalk.yellow( `Moved ${url} back to screenshotQueue` ));
+
+			return false;
+		}
+
+		_self.processingQueue.push( job );
+		console.log(`Awaiting screenshots for ${opts.url} from ${browsers.length} browser(s).`);
+	});
+}
+
+
+
+/**
+ * [saveScreenshots description]
+ * @param  {array} screenshots
+ * @return {[type]}             [description]
+ */
+Browsersnap.prototype.saveScreenshots = function(screenshots) {
+
+	let _self = this;
+
+	screenshots.map(function(screenshot) {
+
+		_self.log(`Saving image for: ${screenshot.url}`);
+
+		if (!fs.existsSync(_self.imagesOutputTo) ) {
+			fs.mkdir(_self.imagesOutputTo);
+		}
+
+		var filename = _.compact( _.values( _.pick( screenshot, ['os','os_version','device','browser','browser_version']))).join('_').toLowerCase();
+
+		got.stream(screenshot.image_url).pipe( fs.createWriteStream( _self.imagesOutputTo + '/' + urlToFilename( screenshot.url ) + '-' + filename +'.jpg',{
+			autoclose : false
+		}));
+
+	});
+}
+
+var urlToFilename = ( url ) => {
+	return url.replace(/https?:\/\/|\/$/g,'').split('?').shift().replace(/\//g,'-');
+};
+
+module.exports = Browsersnap;
